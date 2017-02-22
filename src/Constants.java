ea1896eb924bb0e660fbea75c7eaf6bc3a3b7055
RaymondP <pedersen.r90@gmail.com>
2017-02-22 16:16:43 +0100

    Versjon 0.3

---
+++ src/Constants.java
@@ -0,0 +1,13 @@
+public interface Constants {
+	
+	void setFirstName(String firstName);
+	String getFirstName();
+	void setLastName(String lastName);
+	String getLastName();
+	void setFullName(String fullName);
+	String getFullName();
+	
+	double calculateCredit();
+	double calculateBonus();
+
+}
